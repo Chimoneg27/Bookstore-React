cd3e3ea272cb962c6dd7dc5180341ab73e284097
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-09-22 00:46:26 +0200

    Add components for the bookstore

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { BiSolidUser } from 'react-icons/bi';
+import '../styles/Navigation.css';
+
+const Navbar = () => (
+  <nav className="navigation">
+    <h2>Bookstore CMS</h2>
+    <ul className="items">
+      <li><NavLink to="/">Home</NavLink></li>
+      <li><NavLink to="/category">Categories</NavLink></li>
+    </ul>
+
+    <div>
+      <BiSolidUser className="text-2xl" />
+    </div>
+  </nav>
+);
+
+export default Navbar;

b8d57ace83aa3a4da531a4a146dc77da5d1ed620
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-09-26 21:41:33 +0200

    Move react router to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './components/Home';
 import Categories from './components/Categories';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/category" element={<Categories />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/category" element={<Categories />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

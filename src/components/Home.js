aff688497ecdc4748680b1419d387eb7914d6b1a
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-09-26 21:45:11 +0200

    Generate book items

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/booksSlice';
 import Navbar from './Navbar';
 import BookActions from './BookAction';
 
-const Home = () => (
-  <div className="home-page">
-    <Navbar />
-    <h1>Welcome Home</h1>
-    <BookActions />
-  </div>
-);
+const Home = () => {
+  const book = useSelector((state) => state.books.books);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="home-page">
+      <Navbar />
+      <h1>Welcome Home</h1>
+      <div className="bookshelf">
+        {book.map((book) => (
+          // eslint-disable-next-line camelcase
+          <div className="book" key={book.item_id}>
+            <h3>{ book.title }</h3>
+            <p>{ book.author }</p>
+            <p>{ book.category }</p>
+            <button type="button" onClick={() => dispatch(removeBook(book))}>Remove Book</button>
+          </div>
+        ))}
+      </div>
+      <BookActions />
+    </div>
+  );
+};
 
 export default Home;
